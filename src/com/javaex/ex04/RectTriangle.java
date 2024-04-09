b1f943fd76bd9819d270fcc82e7b4180afb10db1
PARK YESUNG <cjsrnr0504@naver.com>
2024-04-09 16:28:31 +0900

    24.04.09 학원

--- src/com/javaex/ex04/RectTriangle.java
+++ src/com/javaex/ex04/RectTriangle.java
@@ -1,25 +1,38 @@
 package com.javaex.ex04;
 
-public class RectTriangle {
+public class RectTriangle extends Shape {
+	// 필드
     private double width;
     private double height;
+    
+    // 생성자
+    public RectTriangle() {
+    }
+    
+    public RectTriangle(double width, double height) {
+    	this.width = width;
+    	this.height = height;
+    }
     
 	public double getWidth() {
 		return width;
 	}
-	public void setWidth(double width) {
-		this.width = width;
-	}
+
 	public double getHeight() {
 		return height;
 	}
-	public void setHeight(double height) {
-		this.height = height;
-	}
-    
-    public double RectTriangle() {
-    	
-    }
 
+	// 추상클래스(부모) 구현 메소드
+	@Override
+	public double getArea() { // 직각 삼각형 넓이
+		return width * height * 0.5;
+	}
+
+	@Override
+	public double getPerimeter() { // 직각 삼각형 둘레
+		return width + height + Math.sqrt(width * width + height * height); // mat.sqrt가 루트 씌우는 공식
+	}
+
+	
     
 }
--- src/com/javaex/ex04/Rectangle.java
+++ src/com/javaex/ex04/Rectangle.java
@@ -1,22 +1,59 @@
 package com.javaex.ex04;
 
-public class Rectangle implements Resizeable {
+public class Rectangle extends Shape implements Resizeable {
 	
     private double width;
     private double height;
     
-	public double getWidth() {
-		return width;
-	}
+    public Rectangle(double width, double height) {
+    	this.width = width;
+    	this.height = height;
+    }
+    
 	public void setWidth(double width) {
 		this.width = width;
-	}
-	public double getHeight() {
-		return height;
 	}
 	public void setHeight(double height) {
 		this.height = height;
 	}
+
+	@Override
+	public void resize(double s) {
+		width  = width * s;
+		height = height * s;
+	}
+
+	@Override
+	public double sum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int width() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int height() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getArea() {
+		return width * height;
+	}
+
+	@Override
+	public double getPerimeter() {
+		return 2 * (width + height);
+	}
+	
+	
+	
+	
     
     
 
